5606644d55b7a37075b425d3da08e82afe1e6212
Lowie Huyghe <iam@lowiehuyghe.com>
2019-11-07 22:03:26 +0100

    Added test for cli

---
+++ src/cli.test.ts
@@ -0,0 +1,59 @@
+import { expect } from 'chai'
+import * as path from 'path'
+import * as rimraf from 'rimraf'
+import * as glob from 'glob'
+import { execSync } from 'child_process'
+
+const rootDir = path.resolve(__dirname, '..')
+const exampleDir = path.resolve(rootDir, 'example')
+const distDir = path.join(exampleDir, 'dist')
+
+describe('cli', () => {
+  beforeEach(() => {
+    rimraf.sync(distDir)
+  })
+  after(() => {
+    rimraf.sync(distDir)
+  })
+
+  it('fail', () => {
+    expect(() => execSync('./node_modules/.bin/ts-node src/cli.ts -r example', { stdio: 'pipe', cwd: rootDir })).to.throw('Missing required arguments: n, o')
+
+    expect(() => execSync('./node_modules/.bin/ts-node src/cli.ts -r example -n src/app', { stdio: 'pipe', cwd: rootDir })).to.throw('Missing required argument: o')
+    expect(() => execSync('./node_modules/.bin/ts-node src/cli.ts -r example --next src/app', { stdio: 'pipe', cwd: rootDir })).to.throw('Missing required argument: o')
+
+    expect(() => execSync('./node_modules/.bin/ts-node src/cli.ts -r example -o dist', { stdio: 'pipe', cwd: rootDir })).to.throw('Missing required argument: n')
+    expect(() => execSync('./node_modules/.bin/ts-node src/cli.ts -r example --out dist', { stdio: 'pipe', cwd: rootDir })).to.throw('Missing required argument: n')
+  }).timeout(20000)
+
+  it('run', () => {
+    // There should be no dist-folder to start with
+    expect(glob.sync('**/*', { cwd: distDir })).to.deep.equal([])
+
+    execSync('./node_modules/.bin/ts-node src/cli.ts -r example -n src/app -o dist', { stdio: 'pipe', cwd: rootDir })
+
+    // Check the output
+    const contents = glob.sync('**/*', { cwd: distDir })
+      .filter(item => item.indexOf('src/public/_next/') !== 0)
+    expect(contents).to.deep.equal([
+      // root
+      'firebase.json',
+      'src',
+      // functions
+      'src/functions',
+      'src/functions/index.js',
+      'src/functions/package-lock.json',
+      'src/functions/package.json',
+      'src/functions/pages',
+      'src/functions/pages/_error.js',
+      'src/functions/pages/browser.js',
+      // public
+      'src/public',
+      'src/public/_next',
+      'src/public/index.html',
+      'src/public/product',
+      'src/public/product/[pid].html',
+      'src/public/robots.txt'
+    ])
+  }).timeout(20000)
+})
