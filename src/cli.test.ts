cd50de4b05138b5132aa710e0703c80f9d915372
Lowie Huyghe <iam@lowiehuyghe.com>
2020-02-03 16:51:51 +0100

    Also run cli for with-environments-example.

--- src/cli.test.ts
+++ src/cli.test.ts
@@ -8,13 +8,14 @@ const rootDir = path.resolve(__dirname, '..')
 const simpleExampleRelativeDir = path.join('examples', 'simple')
 const simpleExampleDir = path.resolve(rootDir, simpleExampleRelativeDir)
 const simpleDistDir = path.join(simpleExampleDir, 'dist')
+const withEnvironmentsExampleRelativeDir = path.join('examples', 'with-environments')
+const withEnvironmentsExampleDir = path.resolve(rootDir, withEnvironmentsExampleRelativeDir)
+const withEnvironmentsDistDir = path.join(withEnvironmentsExampleDir, 'dist')
 
 describe('cli', () => {
   beforeEach(() => {
     rimraf.sync(simpleDistDir)
-  })
-  after(() => {
-    rimraf.sync(simpleDistDir)
+    rimraf.sync(withEnvironmentsDistDir)
   })
 
   it('fail', () => {
@@ -35,6 +36,37 @@ describe('cli', () => {
 
     // Check the output
     const contents = glob.sync('**/*', { cwd: simpleDistDir })
+      .filter(item => item.indexOf('src/public/_next/') !== 0)
+    expect(contents).to.deep.equal([
+      // root
+      'firebase.json',
+      'src',
+      // functions
+      'src/functions',
+      'src/functions/index.js',
+      'src/functions/package-lock.json',
+      'src/functions/package.json',
+      'src/functions/pages',
+      'src/functions/pages/_error.js',
+      'src/functions/pages/browser.js',
+      // public
+      'src/public',
+      'src/public/_next',
+      'src/public/index.html',
+      'src/public/product',
+      'src/public/product/[pid].html',
+      'src/public/robots.txt'
+    ])
+  }).timeout(10000)
+
+  it('runWithEnvironments', () => {
+    // There should be no dist-folder to start with
+    expect(glob.sync('**/*', { cwd: withEnvironmentsDistDir })).to.deep.equal([])
+
+    execSync(`./cli.js -r ${withEnvironmentsExampleRelativeDir} -n src/app -o dist -e development,staging,production`, { stdio: 'pipe', cwd: rootDir })
+
+    // Check the output
+    const contents = glob.sync('**/*', { cwd: withEnvironmentsDistDir })
       .filter(item => item.indexOf('src/public/_next/') !== 0)
     expect(contents).to.deep.equal([
       // root
